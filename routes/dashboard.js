9c0c0fd93a649a93cb6fc90e761b5a5de9621307
grarenla <s2rainlove2s@gmail.com>
2018-06-13 19:10:50 +0700

    config website-config/contact

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('admin/layouts/master', {path: '/'});
 }).get('/home', function (req, res, next) {
     res.render('admin/pages/home');
 }).get('/user', function (req, res, next) {
     res.render('admin/pages/user');
-}).use('/website-config', require('./website-config'));
+}).use('/website-config', express.Router()
+    .get('/information', function (req, res, next) {
+        res.render('admin/pages/website-config/contact');
+    }));
 
 module.exports = router;
