93a2a2879f43ecccbe68a3dc7312952882f71477
grarenla <s2rainlove2s@gmail.com>
2018-06-19 09:48:34 +0700

    update getOne with View

--- public/admin/js/pages/products-manager/products.js
+++ public/admin/js/pages/products-manager/products.js
@@ -13,13 +13,14 @@ $(function () {
         let products = JSON.parse($(this).attr('data-p'));
         // console.log($('#specificationsbody').html() === "");
         console.log(products.hasOwnProperty('specifications'));
-        $('.productCode-modal').text(products.code);
-        $('.productImg-modal').css('background-image', 'url(' + products.images + ')');
-        $('.productName-modal').text(products.name);
-        $('.productDescription-modal').text(products.description);
-        $('.productCategory-modal').text(products.categories);
-        $('.productBrand-modal').text(products.brand);
-        $('.productPrice-modal').text(products.price + "$");
+        var tableRows = $('#detail-modal > tr');
+        $(tableRows[0]).find('td').last().text(products.code);
+        $(tableRows[1]).find('td').last().find('div').css('background-image', 'url(' + products.images + ')');
+        $(tableRows[2]).find('td').last().text(products.name);
+        $(tableRows[3]).find('td').last().text(products.description);
+        $(tableRows[4]).find('td').last().text(products.categories);
+        $(tableRows[5]).find('td').last().text(products.brand);
+        $(tableRows[6]).find('td').last().text(products.price + "$");
 
             if (products.hasOwnProperty('specifications') === true) {
                 if ($('#specificationsbody').html() === "") {
@@ -38,7 +39,6 @@ $(function () {
         }
         $('#largeModal').modal();
     });
-
 });
 
 function showAjaxLoaderMessage() {
