31c8fc5d224f1b0b8e7d6d84aa06bb72b0490785
grarenla <s2rainlove2s@gmail.com>
2018-06-20 09:17:44 +0700

    add model brand

---
+++ app/models/brand.js
@@ -0,0 +1,31 @@
+'use strict';
+const mongoose = require('mongoose');
+
+let schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    logo: {
+        type: [String],
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    status: {
+        type: Number,
+        required: true
+    }
+});
