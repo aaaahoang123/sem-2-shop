d5d8ad77b579fd3e8d9521f4b63c0d51f16a5116
Hoang <shinkenger.vn9x@gmail.com>
2018-06-23 10:43:00 +0700

    add user manager module

---
+++ app/models/user.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    mid: {
+        type: String,
+        required: true,
+        unique: true,
+        default: Date.now
+    },
+    full_name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    birthday: {
+        type: Date,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    status: {
+        type: Number,
+        required: true,
+        enum: [-1, 1],
+        default: 1
+    }
+});
+
+module.exports = mongoose.model('users', schema);
