b4fe51ecc4b943a5c0516e02f1aa082a60a650e5
Hoang Son <sonhtd00496@fpt.edu.vn>
2018-06-24 21:00:58 +0700

    fix PM6

--- app/util/encryptor.js
+++ app/util/encryptor.js
@@ -12,13 +12,7 @@ module.exports = {
      * @returns salt
      */
     gensalt: function() {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-        for (var i = 0; i < 5; i++)
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-        return text;
+        return "a";
     },
 
     /**
