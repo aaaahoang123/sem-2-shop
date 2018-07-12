a4d2f83b2dd86aa1674f26a80727238bb128a3ae
Hoang <shinkenger.vn9x@gmail.com>
2018-07-12 16:40:34 +0700

    pull

--- public/admin/js/pages/orders/order-form.js
+++ public/admin/js/pages/orders/order-form.js
@@ -6,9 +6,8 @@ $(document).ready(function () {
 
     var searchProductTimeout = null;
     var usedQuery = [];
-    var listProduct = [new Product()];
     var newestProductIndex = 0;
-    $('body').on('change', '.bs-searchbox > input.form-control', function () {
+    $('body').on('keyup', '.bs-searchbox > input.form-control', function () {
         var el = this;
         if (searchProductTimeout !== null) {
             clearTimeout(searchProductTimeout);
@@ -45,8 +44,9 @@ $(document).ready(function () {
         var row = $(this).closest('tr');
         row.attr('data-price', up);
         var quantity = row.find('input.input-quantity').val();
+        row.find('.unit-price').html(up);
         row.find('.total-price').html(up*quantity);
-        console.log(up*quantity);
+        mathTotalPrice();
     });
 
     $('body').on('change', '.input-quantity', function () {
@@ -58,26 +58,31 @@ $(document).ready(function () {
         searchProductTimeout = setTimeout(function () {
             var row = $(el).closest('tr');
             row.find('.total-price').html($(el).val() * parseInt(row.data('price')));
+            mathTotalPrice();
         }, 300);
     });
 
     $('#add-product-btn').click(function () {
         var innerOption = document.querySelector('#product-table select.select-product').outerHTML;
         var row = `
-                   <tr data-index=${++newestProductIndex}>
-                       <th scope="row">${newestProductIndex + 1}</th>
-                       <td>${innerOption}</td>
-                       <td><div class="form-group" style='margin-bottom: 0'><div class="form-line"><input type="number" data-index=${newestProductIndex} class="input-quantity form-control" placeholder="Quantity" value=1></div></div></td>
-                       <td class='total-price' data-index=${newestProductIndex}></td>
-                   </tr>
-               `;
-        // var row = $('<tr>')
-        //     .append($('<th>').html('2'))
-        //     .append($('<td>').append(innerOption))
-        //     .append($('<td>').addClass('total-price').html(0))
-        //  console.log(row);
+           <tr data-index=${++newestProductIndex}>
+               <th scope="row">${newestProductIndex + 1}</th>
+               <td>${innerOption}</td>
+               <td><div class="form-group" style='margin-bottom: 0'><div class="form-line"><input type="number" data-index=${newestProductIndex} class="input-quantity form-control" placeholder="Quantity" value=1></div></div></td>
+               <td class='unit-price' data-index=${newestProductIndex}></td>
+               <td class='total-price' data-index=${newestProductIndex}></td>
+           </tr>
+       `;
         $('#product-table').append(row);
         $('.select-product').selectpicker('refresh');
         $.AdminBSB.input.activate();
-    })
+    });
+
+    const mathTotalPrice = function () {
+        var total = 0;
+        $('.total-price').get().forEach(function (el) {
+            total += parseInt($(el).text());
+        });
+        $('#order-total-price').html(total);
+    }
 });
