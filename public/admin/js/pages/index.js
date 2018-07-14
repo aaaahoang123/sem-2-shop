ae6f4585b189726786fbc59639f194ed357ac200
Hoang <shinkenger.vn9x@gmail.com>
2018-07-14 11:38:55 +0700

    Complete ratio and quantity

--- public/admin/js/pages/index.js
+++ public/admin/js/pages/index.js
@@ -1,8 +1,8 @@
 ﻿function MyChart(option) {
-    this.datatype = (typeof option.datatype === 'undefined')?'order_quantity':option.datatype;
-    this.group = (typeof option.group === 'undefined')?'$dayOfWeek':option.group;
-    this.ofrom = (typeof option.ofrom === 'undefined')?'':option.ofrom;
-    this.oto = (typeof option.oto === 'undefined')?'':option.oto;
+    this.datatype = option.datatype || 'order_quantity-revenue-ratio';
+    this.group = option.group || '$dayOfWeek';
+    this.ofrom = option.ofrom || '';
+    this.oto = option.oto || '';
     this.ORDER_QUANTITY_ELEM = 'bar_chart';
     this.RATIO_ELEM = 'donut_chart';
     this.REVENUE_ELEM = 'line_chart';
@@ -12,57 +12,85 @@ MyChart.prototype = {
 
     order_quantity: function () {
         var self= this;
-        this.Promise.then(function (res) {
-            var data = res[0].order_quantity;
-            var keys = Object.keys(data[0]);
-            $('#'+self.ORDER_QUANTITY_ELEM).html('');
-            Morris.Bar({
-                element: self.ORDER_QUANTITY_ELEM,
-                data: data,
-                xkey: keys[0],
-                ykeys: [keys[1]],
-                barColors: ['rgb(0, 188, 212)'],
-                gridTextColor: ['rgb(233, 30, 99)'],
-                labels: ['Order']
+        this.Promise
+            .then(function (res) {
+                console.log(res);
+                if (res[0].order_quantity.length !== 0) {
+                    var data = res[0].order_quantity;
+                    if (self.group === '$dayOfWeek') {
+                        var dayNames = ['', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+                        data = data.map(function (d) {
+                            d._id = dayNames[d._id];
+                            return d;
+                        });
+                    }
+                    var keys = Object.keys(data[0]);
+                    $('#'+self.ORDER_QUANTITY_ELEM).html('');
+                    Morris.Bar({
+                        element: self.ORDER_QUANTITY_ELEM,
+                        data: data,
+                        xkey: keys[0],
+                        ykeys: [keys[1]],
+                        barColors: ['rgb(0, 188, 212)'],
+                        gridTextColor: ['rgb(233, 30, 99)'],
+                        labels: ['Orders']
+                    });
+                } else {
+                    $('#'+self.ORDER_QUANTITY_ELEM).html('Can not found any data');
+                }
+                return self.Promise;
+            })
+            .catch(function (err) {
+                console.log(err);
+                $('#'+self.ORDER_QUANTITY_ELEM).html('Server error, please check logs and contact with us!');
+                return self.Promise;
             });
-            return self.Promise;
-        });
         return this;
     },
 
     ratio: function() {
         var self = this;
-        this.Promise.then(function (res) {
-            console.log(res);
-            var total = res[0].total_products_quantity[0].quantity;
-            var data = [];
-            var totalPercent = 0;
-            res[0].product_quantity.forEach(function (pr) {
-                var percent = Math.floor((pr.quantity/total)*100);
-                totalPercent += percent;
-                data.push({
-                    label: pr.content[0].name,
-                    value: percent
-                });
-            });
-            if (totalPercent < 100) {
-                data.push({
-                    label: 'Others',
-                    value: 100 - totalPercent
-                })
-            }
+        this.Promise
+            .then(function (res) {
+                console.log(res);
+                if (res[0].total_products_quantity.length !== 0) {
+                    var total = res[0].total_products_quantity[0].quantity;
+                    var data = [];
+                    var totalPercent = 0;
+                    res[0].product_quantity.forEach(function (pr) {
+                        var percent =((pr.quantity/total)*100).toFixed(2);
+                        totalPercent += percent;
+                        data.push({
+                            label: pr.content[0].name,
+                            value: percent
+                        });
+                    });
+                    if (totalPercent < 100) {
+                        data.push({
+                            label: 'Others',
+                            value: 100 - totalPercent
+                        })
+                    }
 
-            $('#'+self.RATIO_ELEM).html('');
-            Morris.Donut({
-                element: self.RATIO_ELEM,
-                data: data,
-                colors: ['rgb(233, 30, 99)', 'rgb(0, 188, 212)', 'rgb(255, 152, 0)', 'rgb(0, 150, 136)'],
-                formatter: function (y) {
-                    return y + '%'
+                    $('#'+self.RATIO_ELEM).html('');
+                    Morris.Donut({
+                        element: self.RATIO_ELEM,
+                        data: data,
+                        colors: ['rgb(233, 30, 99)', 'rgb(0, 188, 212)', 'rgb(255, 152, 0)', 'rgb(0, 150, 136)'],
+                        formatter: function (y) {
+                            return y + '%'
+                        }
+                    });
+                } else {
+                    $('#'+self.RATIO_ELEM).html('Can not found any data');
                 }
+                return self.Promise;
+            })
+            .catch(function (err) {
+                console.log(err);
+                $('#'+self.RATIO_ELEM).html('Server error, please check logs and contact with us!');
+                return self.Promise;
             });
-            return self.Promise;
-        });
         return this;
     },
 
@@ -76,12 +104,8 @@ MyChart.prototype = {
             $.ajax({
                 url: '/api/charts?datatype=' + self.datatype + '&group=' + self.group + '&ofrom=' + self.ofrom + '&oto=' + self.oto,
                 method: 'GET',
-                success: function(res) {
-                    resolve(res);
-                },
-                error: function(err) {
-                    reject(err);
-                }
+                success: resolve,
+                error: reject
             });
         });
         return this;
@@ -92,16 +116,14 @@ var toDay, thisWeekStart, thisMonthStart, lastWeekStart, lastMonthStart;
 
 $(document).ready(function() {
     var now = new Date();
-    var first = now.getDate() - now.getDay() + 1;
+    var first = now.getDate() - now.getDay();
     //console.log(new Date(now.setDate(first)).toISOString());
     toDay = new Date().toISOString().split('T')[0];
     thisWeekStart = new Date(new Date().setDate(first)).toISOString().split('T')[0];
     thisMonthStart = new Date(new Date().setDate(1)).toISOString().split('T')[0];
     lastWeekStart = new Date(new Date().setDate(first-7)).toISOString().split('T')[0];
-    lastMonthStart = new Date(new Date().setMonth(now.getMonth() - 1)).toISOString().split('T')[0];
-
+    lastMonthStart = new Date(new Date(thisMonthStart).setMonth(now.getMonth() - 1)).toISOString().split('T')[0];
     new MyChart({
-        datatype: 'order_quantity-ratio',
         ofrom: thisWeekStart,
         oto: toDay
     }).load().order_quantity().ratio();
